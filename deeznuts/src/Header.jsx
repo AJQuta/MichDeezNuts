e0db56d81e14c49b5f535bfc08ea0d9ed446a0a7
Michael Lynch <ml849@hotmail.com>
2025-02-07 12:37:10 -0500

    Added a MUI Button

--- deeznuts/src/Header.jsx
+++ deeznuts/src/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
-
+import { Button } from '@mui/material';
 function Header() {
+
+    const handleClick = () => {
+        alert('hi');
+    };
+
 
     return <header>
         Epic header that totally works
+        <Button onClick={handleClick} color={"primary"}>
+            Material Button
+        </Button>
     </header>
 
 } export default Header
